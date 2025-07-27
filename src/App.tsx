57ab33e4b6616a793f4d7c36518da4859c6fff3d
AndreasSoularidis <andreas.soularidis@gmail.com>
2025-07-27 14:28:44 +0300

    basic layout implementation

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,92 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React from 'react';
+import { CalendarOutlined, LaptopOutlined, NotificationOutlined, TeamOutlined, UserOutlined } from '@ant-design/icons';
+import type { MenuProps } from 'antd';
+import { Layout, Menu } from 'antd';
 import './App.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSchool } from '@fortawesome/free-solid-svg-icons/faSchool';
+import { faCalendarWeek, faChalkboardUser, faHouse, faPeopleGroup, faPersonChalkboard, faUser } from '@fortawesome/free-solid-svg-icons';
+
+const { Header, Content, Sider } = Layout;
+
+
+const navBarItems: MenuProps['items'] = [
+  {
+    key: "user",
+    icon: <FontAwesomeIcon icon={faUser} />
+  }
+]
+
+const sideBarMenuItems: MenuProps['items'] = [
+  {
+    key: "home",
+    icon: <FontAwesomeIcon icon={faHouse} />,
+    label: "Αρχική"
+  },
+  {
+    key: "school",
+    icon: <FontAwesomeIcon icon={faSchool} />,
+    label: "Σχολική Μονάδα"
+  },
+  {
+    key: "groups",
+    icon: <FontAwesomeIcon icon={faPeopleGroup}/>,
+    label: "Τμήματα"
+  },
+  {
+    key: "teachers",
+    icon: <FontAwesomeIcon icon={faChalkboardUser}/>,
+    label: "Εκπαιδευτικοί"
+  },
+  {
+    key: "teachings",
+    icon: <FontAwesomeIcon icon={faPersonChalkboard} />,
+    label: "Διδασκαλίες"
+  },
+  {
+    key: "timetables",
+    icon: <FontAwesomeIcon icon={faCalendarWeek} />,
+    label: "Προγράμματα"
+  },
+
+]
+
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <Layout style={{minHeight: "100vh", padding: 0}}>
+      <Header style={{ display: 'flex', justifyContent:"space-between", alignItems: 'center' }}>
+        <div className="demo-logo">iTimetables</div>
+        <Menu
+          theme="dark"
+          mode="horizontal"
+          items={navBarItems}
+          style={{ display: "flex", justifyContent:"flex-end", flex: 8, minWidth: 0, }}
+        />
+      </Header>
+      <Layout>
+        <Sider width={230}>
+          <Menu
+            mode="inline"
+            defaultSelectedKeys={['home']}
+            defaultOpenKeys={['home']}
+            style={{ height: '100%', fontSize: "1rem",  borderRight: 0 }}
+            items={sideBarMenuItems}
+          />
+        </Sider>
+        <Layout style={{ padding: '0 24px 24px' }}>
+          <Content
+            style={{
+              padding: 24,
+              margin: 0,
+              minHeight: 280,
+            }}
+          >
+            Content
+          </Content>
+        </Layout>
+      </Layout>
+    </Layout>
   )
 }
 
