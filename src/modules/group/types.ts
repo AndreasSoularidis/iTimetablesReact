cfebcc2ac7224a084d04c10d1c814e71ea0dd84f
AndreasSoularidis <andreas.soularidis@gmail.com>
2026-07-30 20:13:14 +0300

    teaching functionality removed from groups

--- src/modules/group/types.ts
+++ src/modules/group/types.ts
@@ -42,31 +42,3 @@ export interface GroupEntity {
     schoolUnitId: string;
 }
 
-export interface Courses{
-    id: string;
-    title: string;
-    short: string;
-    grade: Grade;
-}
-
-export interface Grade{
-    id: string;
-    description: string;
-    hoursPerWeek: number;
-}
-
-export interface TeachingPost{
-    schoolClassId: string;
-    teacherId: string;
-    courseId: string;
-    totalHours: number;
-    dispersion: number[];
-}
-
-export interface TeachingEntity{
-    key: string;
-    teacher: LookUp;
-    course: LookUp;
-    totalHours: number;
-    dispersion: number[];
-}
